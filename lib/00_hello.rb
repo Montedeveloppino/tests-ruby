0c73cb86bbd4cf63bd2fd743e0568876e835c2e7
Youssef <annabiyoussef1986@gmail.com>
2021-01-18 08:46:00 +0100

    success exo1

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,18 @@
+alice = "Alice"
+bob = "Bob"
+
 def hello
-
+return "Hello!"
 end
 
-def greet(name)
-
+def greet(alice)
+  return "Hello, #{alice}!"
 end
+
+def greet(bob)
+  return "Hello, #{bob}!"
+end
+
+puts hello
+puts greet(alice)
+puts greet(bob)
