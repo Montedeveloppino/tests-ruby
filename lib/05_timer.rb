ae707d1958aaf7a2b6e107ac0574d4352efb44e0
Youssef <annabiyoussef1986@gmail.com>
2021-01-18 19:30:01 +0100

    adding time training

---
+++ lib/05_timer.rb
@@ -0,0 +1,4 @@
+def time_string(nb)
+  t = Time.new(0)
+  return (t + nb).strftime("%H:%M:%S")
+end
--- lib/test.rb
+++ lib/test.rb
@@ -1,16 +1,5 @@
-
-
-#print str.split(/ |\_|\-/).map(&:capitalize).join(" ")
-def string_upc
-  str = "bonjour tout le monde a vous"
-tab = str.split(" ")
-  tab.each do |x| 
-    if(x.eql?(tab[0]) || x.length > 3) 
-      x.capitalize!
-    end
-  end
-  return tab.join(" ")
+def time_string(nb)
+  t = Time.new(0)
+  return (t + nb).strftime("%H:%M:%S")
 end
-
-
-print string_upc
+puts time_string(nb)
